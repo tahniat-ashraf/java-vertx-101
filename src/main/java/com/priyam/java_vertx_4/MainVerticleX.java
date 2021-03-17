157468abb4ebb6c8d2567942e957059801b2955b
Tahniat Ashraf Priyam <tahniat88@gmail.com>
2021-03-17 21:53:26 +0600

    Fix getAllKeyValues

--- src/main/java/com/priyam/java_vertx_4/MainVerticleX.java
+++ src/main/java/com/priyam/java_vertx_4/MainVerticleX.java
@@ -10,14 +10,6 @@ public class MainVerticleX extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
 
-//    vertx.deployVerticle(new Subscriber1X());
-//    vertx.deployVerticle(new Subscriber2X());
-//    vertx.deployVerticle(new P2PConsumer1X());
-//    vertx.deployVerticle(new P2PSenderX());
-//    vertx.deployVerticle(new PublisherX());
-//    vertx.deployVerticle(new Router1());
-//    vertx.deployVerticle(new StudentCrudVerticle());
-//    vertx.deployVerticle("com.priyam.java_vertx_4.client.DepartmentApiWebClient");
     vertx.deployVerticle(new KeyValueServiceVerticle());
     vertx.deployVerticle(new KeyValueController());
 
